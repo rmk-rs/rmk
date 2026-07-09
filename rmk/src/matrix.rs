cf442fd3ab3bbedf0b98fb89ace2185ee6ed112a
Haobo Gu <haobogu@outlook.com>
2026-07-09 22:56:25 +0800

    fix(matrix): avoid unsafe direct pin wait selection

--- rmk/src/matrix.rs
+++ rmk/src/matrix.rs
@@ -1,11 +1,9 @@
-#[cfg(feature = "async_matrix")]
-use core::pin::pin;
 use core::sync::atomic::Ordering;
 
 use embassy_time::Timer;
 use embedded_hal::digital::{InputPin, OutputPin};
 #[cfg(feature = "async_matrix")]
-use {embassy_futures::select::select_slice, embedded_hal_async::digital::Wait, heapless::Vec};
+use {embassy_futures::select::select_array, embedded_hal_async::digital::Wait};
 
 use crate::CONNECTION_STATE;
 use crate::debounce::{DebounceState, DebouncerTrait};
@@ -281,12 +279,8 @@ impl<
 
     #[cfg(feature = "async_matrix")]
     async fn wait_input_pins(&mut self) {
-        let mut futs: Vec<_, ROW> = self
-            .get_input_pins_mut()
-            .iter_mut()
-            .map(|input_pin| input_pin.wait_for_high())
-            .collect();
-        let _ = select_slice(pin!(futs.as_mut_slice())).await;
+        let futs = self.row_pins.each_mut().map(|input_pin| input_pin.wait_for_high());
+        let _ = select_array(futs).await;
     }
 }
 
@@ -309,12 +303,8 @@ impl<
 
     #[cfg(feature = "async_matrix")]
     async fn wait_input_pins(&mut self) {
-        let mut futs: Vec<_, COL> = self
-            .get_input_pins_mut()
-            .iter_mut()
-            .map(|input_pin| input_pin.wait_for_high())
-            .collect();
-        let _ = select_slice(pin!(futs.as_mut_slice())).await;
+        let futs = self.col_pins.each_mut().map(|input_pin| input_pin.wait_for_high());
+        let _ = select_array(futs).await;
     }
 }
 
